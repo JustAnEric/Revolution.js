eda13d529e8ded0cf33ee44bbfa5d4579f2df5dd
MeowyCats2 <meowycats2@protonmail.com>
2023-07-10 05:37:29 +0000

    Added message and channel class, and command handling

--- lib.js
+++ lib.js
@@ -14,10 +14,16 @@ const Color = {
 }
 
 export class commands {
-  constructor () {
+  constructor (bot, prefix) {
+    this.bot = bot
+    this.prefix = prefix
+    this.commands = []
+  }
+  command (name, parameters, callback, config) {
+    this.commands.push({name, parameters, callback, config})
   }
   static async Bot () {
-    console.log(`${Color.Warning}Commands: ${Color.EndC}${Color.OkBlue}Commands is deprecated. Please use the Bot class directly.`)
+    console.log(`${Color.Warning}Commands: ${Color.EndC}${Color.OkBlue}Commands.Bot is deprecated. Please use the Bot class directly.`)
     return Bot
   }
 }
@@ -32,6 +38,9 @@ export class Bot {
     }
     this.endpoint = config.endpoint || "https://revolution-web.repl.co"
     this.socketURL = config.socketURL || "wss://revolution-web.repl.co"
+    if (config.commands) {
+      this.commands = new commands(this, config.prefix)
+    }
   }
   listen (name, func) {
     this.events.push({"type": name, func})
@@ -81,21 +90,95 @@ export class Bot {
       const obj = JSON.parse(data)
       if (obj.type === "messageCreate") {
         try {
-          this.events.filter(c => c.type === "server_message").forEach(c => c.func({"message": obj.message, "sent_by": obj.sent_by, "channel": obj.channel}))
+          this.events.filter(c => c.type === "server_message").forEach(c => c.func(new Message(obj, this)))
         } catch (e) {
           console.log(`${Color.Warning}Error while running event:\n${e.stack}${Color.EndC}`)
+        }
+        if (this.commands && obj.message.startsWith(this.commands.prefix)) {
+          process_commands(this, obj)
         }
       }
     });
   }
-  async send_message (server, message) {
+  async send_message (channel, message) {
     const sendReq = await fetch(this.endpoint + "/api/v1/servers/send_message", {
       method: "GET",
       headers: {
-        id: server,
+        id: channel,
         message,
         sent_by: this.bot.name,
         token: this.token
+      }
+    })
+    const sendRes = await sendReq.json()
+    return sendRes
+  }
+}
+
+const process_commands = (bot, obj) => {
+          const commandParts = obj.message.split(" ");
+          const commandName = commandParts[0].split(bot.commands.prefix)[1]
+          const parameters = [];
+  
+          let arg = "";
+          let isArgument = false;
+  
+          for (let i = 1; i < commandParts.length; i++) {
+            if (commandParts[i].startsWith("\"") && !isArgument) {
+              isArgument = true;
+              arg += commandParts[i].substring(1);
+            } else if (commandParts[i].endsWith("\"") && isArgument) {
+              isArgument = false;
+              arg += ` ${commandParts[i].substring(0, commandParts[i].length-1)}`;
+              parameters.push(arg);
+              arg = "";
+            } else if (isArgument) {
+              arg += ` ${commandParts[i]}`;
+            } else {
+              parameters.push(commandParts[i]);
+            }
+          }
+          const command = bot.commands.commands.filter(c => c.name === commandName)[0]
+          let result = {}
+          for (let [i, param] of Object.entries(parameters)) {
+            if (i > command.parameters.length - 1) break
+            result[command.parameters[i]] = param
+          }
+          command.callback(new Message(obj, this), result)
+}
+
+export class Message {
+  constructor (obj, bot) {
+    this.message = obj.message
+    this.sent_by = obj.sent_by
+    this.channel = new Channel(obj.channel, bot)
+    this.bot = bot
+  }
+}
+
+export class Channel {
+  constructor (id, bot) {
+    this.id = id
+    this.bot = bot
+  }
+  async send_message (message) {
+    const sendReq = await fetch(this.endpoint + "/api/v1/servers/send_message", {
+      method: "GET",
+      headers: {
+        id: this.id,
+        message,
+        sent_by: bot.bot.name,
+        token: bot.token
+      }
+    })
+    const sendRes = await sendReq.json()
+    return sendRes
+  }
+  async fetch_messages () {
+    const sendReq = await fetch(this.endpoint + "/api/v1/get_server_messages", {
+      method: "GET",
+      headers: {
+        id: this.id
       }
     })
     const sendRes = await sendReq.json()
